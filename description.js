0cd3ad9140fceeb7e233c292315ee3de9f588a66
Mario <marioastur@gmail.com>
2022-05-31 22:55:43 +0200

    Mapas de salas

--- description.js
+++ description.js
@@ -9,21 +9,3 @@ console.log(roomDescription['sala0']['descripcion'])
 console.log(roomDescription['sala0']['objeto'])
 
 // Matriz de salas
-
-let miSala = [
-    [0, 1, 2],
-    [0, 1, 2],
-    [0, 1, 2]
-]
-
-miSala[0][0] = 'Descripción Sala 1'
-miSala[0][1] = 'Descripción Sala 2'
-miSala[0][2] = 'Descripción Sala 3'
-
-miSala[1][0] = 'Descripción Sala 4'
-miSala[1][1] = 'Descripción Sala 5'
-miSala[1][2] = 'Descripción Sala 6'
-
-miSala[2][0] = 'Descripción Sala 7'
-miSala[2][1] = 'Descripción Sala 8'
-miSala[2][2] = 'Descripción Sala 9'
--- juego.js
+++ juego.js
@@ -1,27 +1,31 @@
-let gameVerbs = new Map([
-    ['ir', true],
-    ['coger', true],
-    ['usar', true],
-    ['ver', true],
-    ['hablar', true]
-])
-
+const gameMap = createMap()
+const gameVerbs = createVerbs()
 let textDescription = new String
 
+// INICIO DE JUEGO
+let player = { position: [1, 2] }
+positionPlayer(player.position[0], player.position[1])
 
+
+// CAPTURA DE EVENTOS
 const orderButton = document.getElementById('do')
-orderButton.onclick = function() {
+orderButton.onclick = eventGame
+
+
+//#############################################
+//############  F U N C I O N E S  ############
+//#############################################
+
+function eventGame() {
 
     let command = document.getElementById('commands').value
     let verbArray = separateWords(command.toLowerCase())
-    let getVerb = isGameVerbs(verbArray[0])
-    if (getVerb == false) {
+    if (isGameVerbs(verbArray[0]) == false) {
         printDescription('Orden no válida. Use ir,coger,usar,ver,hablar')
     } else {
         printDescription(gameAccions(verbArray[0]))
     }
 }
-
 
 function printDescription(text) {
     let today = new Date()
@@ -30,6 +34,27 @@ function printDescription(text) {
     document.getElementById('description').innerHTML = textDescription
 }
 
+function separateWords(words) {
+    return words.split(' ')
+}
+
+function createVerbs() {
+    const verbs = new Map([
+        ['ir', true],
+        ['coger', true],
+        ['usar', true],
+        ['ver', true],
+        ['hablar', true]
+    ])
+    return verbs
+}
+
+function isGameVerbs(verbArray) {
+    if (gameVerbs.has(verbArray)) {
+        return true
+    }
+    return false
+}
 
 function gameAccions(verbGame) {
     switch (verbGame) {
@@ -53,15 +78,38 @@ function gameAccions(verbGame) {
     }
 }
 
+function createMap() {
+    const miSala = [
+        [0, 1, 2, 3],
+        [0, 1, 2, 3],
+        [0, 1, 2, 3],
+        [0, 1, 2, 3]
+    ]
 
-function separateWords(words) {
-    return words.split(' ')
+    miSala[0][0] = { description: 'Descripción Sala 00', exits: ['N'] }
+    miSala[0][1] = { description: 'Descripción Sala 01', exits: ['E', 'W'] }
+    miSala[0][2] = { description: 'Descripción Sala 02', exits: ['N', 'E', 'W'] }
+    miSala[0][3] = { description: 'Descripción Sala 03', exits: ['E'] }
+
+    miSala[1][0] = { description: 'Descripción Sala 10', exits: ['N', 'S'] }
+    miSala[1][1] = { description: 'Descripción Sala 11', exits: [''] }
+    miSala[1][2] = { description: 'Descripción Sala 12', exits: ['S', 'E'] }
+    miSala[1][3] = { description: 'Descripción Sala 13', exits: ['N', 'W'] }
+
+    miSala[2][0] = { description: 'Descripción Sala 20', exits: ['S', 'E'] }
+    miSala[2][1] = { description: 'Descripción Sala 21', exits: ['E', 'W'] }
+    miSala[2][2] = { description: 'Descripción Sala 22', exits: ['N', 'E', 'W'] }
+    miSala[2][3] = { description: 'Descripción Sala 23', exits: ['N', 'W'] }
+
+    miSala[3][0] = { description: 'Descripción Sala 30', exits: ['E'] }
+    miSala[3][1] = { description: 'Descripción Sala 31', exits: ['E', 'W'] }
+    miSala[3][2] = { description: 'Descripción Sala 32', exits: ['S', 'E', 'W'] }
+    miSala[3][3] = { description: 'Descripción Sala 33', exits: ['S', 'W'] }
+
+    return miSala
 }
 
-
-function isGameVerbs(verbArray) {
-    if (gameVerbs.has(verbArray)) {
-        return true
-    }
-    return false
+function positionPlayer(x, y) {
+    let descriptionRoom = createMap()[x][y].description
+    printDescription(descriptionRoom)
 }
